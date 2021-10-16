52429312bf9800de290c105978e63c2fda62c888
anyavera <neverova.anya@gmail.com>
2021-10-15 17:51:01 -0700

    h
    m

---
+++ index.js
@@ -0,0 +1,5 @@
+iimport React from "react"
+import ReactDOM from "react-dom"
+
+//ReactDOM.render(WHAT DO I WANT TO RENDER, WHERE DO I WANT TO RENDER IT)
+ReactDOM.render(<h1>Anna Neverovas</h1>, document.getElementById("root"))
